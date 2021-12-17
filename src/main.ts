d099e08bec352c9009b47216d3831f5cd8b28dfd
leo.yu <leo.yu@choiceform.com>
2021-12-17 10:07:41 +0800

    feat: add cros

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+
+const envConf = dotenv.parse(fs.readFileSync('.env'));
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('v1');
+  app.enableCors();
+  await app.listen(envConf['PORT']);
 }
 bootstrap();
