072cdb9c90fd5fb85e15fde38a9b3a10aa0739fa
leo.yu <leo.yu@choiceform.com>
2022-01-06 18:49:04 +0800

    feat: add test #1

--- src/modules/push-log/push-log.service.spec.ts
+++ src/modules/push-log/push-log.service.spec.ts
@@ -1,12 +1,46 @@
+import { Provider } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { AnswerService } from 'src/common/service/answer.service';
+import { Repository } from 'typeorm';
+import { AnswerParamsService } from '../answer-params/answer-params.service';
+import { QueueService } from '../queue/queue.service';
+import { User } from '../user/entities/user.entity';
+import { PushLog } from './entities/push-log.entity';
 import { PushLogService } from './push-log.service';
 
 describe('PushLogService', () => {
   let service: PushLogService;
+  let userRepository: Repository<User>;
+  let pushLogRepository: Repository<PushLog>;
+
+  const QueueProvider: Provider = {
+    provide: QueueService,
+    useFactory: () => ({}),
+  };
+  const AnswerProvider: Provider = {
+    provide: AnswerService,
+    useFactory: () => ({}),
+  };
+  const AnswerParamProvider: Provider = {
+    provide: AnswerParamsService,
+    useFactory: () => ({
+      dispose: jest.fn(() => {
+        return {
+          eatDate: '',
+          mobile: '',
+        };
+      }),
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PushLogService],
+      providers: [
+        PushLogService,
+        AnswerProvider,
+        QueueProvider,
+        AnswerParamProvider,
+      ],
     }).compile();
 
     service = module.get<PushLogService>(PushLogService);
