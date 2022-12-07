6aa6ee8f96da7f97cfb70fa0532c9ed23c07f5a1
leo.yu <leo.yu@choiceform.com>
2022-12-07 11:36:46 +0800

    feat: 添加通用推送接口

--- src/modules/push/push.controller.ts
+++ src/modules/push/push.controller.ts
@@ -3,12 +3,20 @@ import { PushService } from './push.service';
 
 @Controller('push')
 export class PushController {
-  constructor(private readonly pushService: PushService) {}
+  constructor(private readonly pushService: PushService) { }
 
   @Post('')
   async push(@Body() body: { mobile: string }) {
     const { mobile } = body;
     const content = `【凯迪拉克】LYRIQ预订用户专享！参与LYRIQ用户调研获500 IQ积分。点击链接，立刻参与：https://q.cform.io/?cWXbF7Ns 回T退订`;
+    return this.pushService.pushTemp({
+      mobile,
+      content,
+    });
+  }
+  @Post('mobile')
+  async PushMobileMessage(@Body() body: { mobile: string; content: string }) {
+    const { mobile, content } = body;
     return this.pushService.pushTemp({
       mobile,
       content,
